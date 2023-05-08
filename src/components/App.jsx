0b46a873236cecaa038440e09175950fa7b4c045
Roman Lebedynskyi <108210477+Anasurim@users.noreply.github.com>
2023-05-08 18:16:12 +0200

    added an option t osave contact list to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,22 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = contact => {
     const newContact = {
       id: nanoid(5),
